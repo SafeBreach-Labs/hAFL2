31d7d399ca9d16637b2942951994ca2a5a12a95f
Peleg <peleg.hadar.w@gmail.com>
2021-07-16 15:49:29 +0300

    Changed Harness executable

--- targets/windows_x86_64/src/fuzzer/packet_sender.c
+++ targets/windows_x86_64/src/fuzzer/packet_sender.c
@@ -23,45 +23,45 @@ along with QEMU-PT.  If not, see <http://www.gnu.org/licenses/>.
 #include <stdio.h>
 #include "kafl_user.h"
 
-#define IOCTL_KAFL_INPUT    (ULONG) CTL_CODE(FILE_DEVICE_UNKNOWN, 0x800, METHOD_NEITHER, FILE_ANY_ACCESS)
+#define IOCTL_SEND_PACKET CTL_CODE(FILE_DEVICE_UNKNOWN, 0x1, METHOD_BUFFERED, FILE_WRITE_ACCESS)
+
 
 int main(int argc, char** argv)
 {
     kAFL_payload* payload_buffer = (kAFL_payload*)VirtualAlloc(0, PAYLOAD_SIZE, MEM_COMMIT, PAGE_READWRITE);
-    //LPVOID payload_buffer = (LPVOID)VirtualAlloc(0, PAYLOAD_SIZE, MEM_COMMIT, PAGE_READWRITE);
     memset(payload_buffer, 0xff, PAYLOAD_SIZE);
 
     /* open vulnerable driver */
-    HANDLE kafl_vuln_handle = NULL;
+    HANDLE hHarness = NULL;
     BOOL status = -1;
-    kafl_vuln_handle = CreateFile((LPCSTR)"\\\\.\\testKafl",
-        GENERIC_READ | GENERIC_WRITE,
-        FILE_SHARE_READ | FILE_SHARE_WRITE,
+    
+    hHarness = CreateFile((LPCSTR)"\\\\.\\CPHarness",
+        GENERIC_WRITE,
+        0,
         NULL,
         OPEN_EXISTING,
-        FILE_ATTRIBUTE_NORMAL | FILE_FLAG_OVERLAPPED,
+        0,
         NULL
     );
 
-    if (kafl_vuln_handle == INVALID_HANDLE_VALUE) {
-        printf("[-] KAFL test: Cannot get device handle: 0x%X\n", GetLastError());
+    if (hHarness == INVALID_HANDLE_VALUE) {
+        hprintf("[-] hAFL2 harness: Cannot get device handle: 0x%X\n", GetLastError());
         ExitProcess(0);
     }
-
-    /* this hypercall submits the current CR3 value */ 
-    kAFL_hypercall(HYPERCALL_KAFL_SUBMIT_CR3, 0);
 
     /* submit the guest virtual address of the payload buffer */
     kAFL_hypercall(HYPERCALL_KAFL_GET_PAYLOAD, (UINT64)payload_buffer);
 
+    /* Warning: This part won't work well unless you'll patch the VMSwitch packet signal mechanism.
+    For more information, read the "VMSwitch Harness Gaps" section within the README.md file of hAFL2. */
     while(1){
             kAFL_hypercall(HYPERCALL_KAFL_NEXT_PAYLOAD, 0);
             /* request new payload (*blocking*) */
             kAFL_hypercall(HYPERCALL_KAFL_ACQUIRE, 0); 
-
             /* kernel fuzzing */
-            DeviceIoControl(kafl_vuln_handle,
-                IOCTL_KAFL_INPUT,
+            hprintf("Sending payload with size: 0x%x", payload_buffer->size);
+            DeviceIoControl(hHarness,
+                IOCTL_SEND_PACKET,
                 (LPVOID)(payload_buffer->data),
                 (DWORD)payload_buffer->size,
                 NULL,
@@ -69,7 +69,6 @@ int main(int argc, char** argv)
                 NULL,
                 NULL
             );
-
             /* inform fuzzer about finished fuzzing iteration */
             kAFL_hypercall(HYPERCALL_KAFL_RELEASE, 0);
     }
